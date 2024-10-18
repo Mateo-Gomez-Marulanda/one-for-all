996c750abcc46f64c729cc2c35ae7bfa5be988d5
Mateo-Gomez-Marulanda <mateo.gomezm@uqvirtual.edu.co>
2024-10-17 20:39:54 -0500

    update

---
+++ demo/src/main/java/com/example/PatronesComportamentales/ChainOfResponsability/taller0/Main.java
@@ -0,0 +1,43 @@
+package com.example.PatronesComportamentales.ChainOfResponsability.taller0;
+
+abstract class HandlerBase{
+    protected HandlerBase handlerBase;
+
+    public void handlerRequest(int level, String message){
+        if(handlerBase != null){
+            handlerBase.handlerRequest(level, message);
+        }
+    }
+}
+
+//verifica si es de nivel 1
+class Level1Handler extends HandlerBase{
+    
+}
+
+class Logger {
+
+    public void logMessage(int level, String message) {
+        if (level == 1) {
+            System.out.println("info: " + message);
+        }
+
+        if (level == 2) {
+            System.out.println("info: " + message);
+        }
+
+        if (level == 3) {
+            System.out.println("info: " + message);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = new Logger();
+        logger.logMessage(1, "mondongo");
+        logger.logMessage(2, "hola");
+        logger.logMessage(1, "adios");
+    }
+}
